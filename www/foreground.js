5440c66168aacdab24d7a50137314a5c3d64dfb2
21pg <64975861+21pg@users.noreply.github.com>
2020-08-06 16:24:52 +0530

    Update foreground.js

--- www/foreground.js
+++ www/foreground.js
@@ -1,8 +1,8 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  start: function(title, text, icon, importance, notificationId) {
-    exec(null, null, "ForegroundPlugin", "start", [title || "", text || "", icon || "", importance || "1", notificationId || ""]);
+  start: function(title, text, importance, notificationId) {
+    exec(null, null, "ForegroundPlugin", "start", [title || "", text || "", importance || "1", notificationId || ""]);
   },
   stop: function() {
     exec(null, null, "ForegroundPlugin", "stop", []);
